19d722353d767ff42dbb5c4d204f819627e884e5
ShenDezhou <tsinghua9boy@sina.com>
2021-08-23 15:36:41 +0800

    1.train bert.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,18 +2,18 @@
 #assert os.environ['COLAB_TPU_ADDR'], 'Make sure to select TPU from Edit > Notebook settings > Hardware accelerator'
 
 DIST_BUCKET="gs://tpu-pytorch/wheels"
-TORCH_WHEEL="torch-1.15-cp37-cp37m-linux_x86_64.whl"
-TORCH_XLA_WHEEL="torch_xla-1.15-cp37-cp37m-linux_x86_64.whl"
-TORCHVISION_WHEEL="torchvision-0.3.0-cp37-cp37m-linux_x86_64.whl"
+TORCH_WHEEL="torch-1.9-cp37-cp37m-linux_x86_64.whl"
+TORCH_XLA_WHEEL="torch_xla-1.9-cp37-cp37m-linux_x86_64.whl"
+#TORCHVISION_WHEEL="torchvision-0.3.0-cp37-cp37m-linux_x86_64.whl"
 
 # Install Colab TPU compat PyTorch/TPU wheels and dependencies
 pip uninstall -y torch torchvision
 gsutil cp "$DIST_BUCKET/$TORCH_WHEEL" .
 gsutil cp "$DIST_BUCKET/$TORCH_XLA_WHEEL" .
-gsutil cp "$DIST_BUCKET/$TORCHVISION_WHEEL" .
+#gsutil cp "$DIST_BUCKET/$TORCHVISION_WHEEL" .
 pip install "$TORCH_WHEEL"
 pip install "$TORCH_XLA_WHEEL"
-pip install "$TORCHVISION_WHEEL"
+#pip install "$TORCHVISION_WHEEL"
 sudo apt-get install libomp5
 
 VERSION="20200516"  # @param ["1.5" , "20200516", "nightly"]
